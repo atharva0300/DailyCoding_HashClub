ca4d44380f4ecfd7a27c51efdbc71fdc1fece86a
Atharva Pingale <76089814+atharva0300@users.noreply.github.com>
2022-01-10 00:03:01 +0530

    Day 7 solution aded

---
+++ Day-7_trapping-rain-water.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int h=height.size();
+        vector<int>left(h,0);
+        vector<int>right(h,0);
+        
+        int temp=0;
+        int result=0;
+        for(int i=0; i<h;++i){
+            temp=max(temp,height[i]);
+            left[i]=temp;
+        }
+        temp=0;
+        for(int j=h-1;j>=0;--j){
+            temp=max(temp,height[j]);
+            right[j]=temp;
+        }
+        
+        for(int i=1; i<h-1;++i){
+            int temp=min(left[i-1],right[i+1])-height[i];
+            if(temp>0){
+                result+=temp;
+            }
+        }
+        return result;
+    }
+};
