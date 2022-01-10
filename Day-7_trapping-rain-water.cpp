c018edd470a78fa4a9529a3f01b090a4cdb8ba63
Atharva Pingale <76089814+atharva0300@users.noreply.github.com>
2022-01-10 18:15:00 +0530

    extra line deleated

--- Day-7_trapping-rain-water.cpp
+++ Day-7_trapping-rain-water.cpp
@@ -7,17 +7,17 @@ public:
         
         int temp=0;
         int result=0;
-        for(int i=0; i<h;++i){
+        for(int i=0; i<h;i++){
             temp=max(temp,height[i]);
             left[i]=temp;
         }
         temp=0;
-        for(int j=h-1;j>=0;--j){
+        for(int j=h-1;j>=0;j--){
             temp=max(temp,height[j]);
             right[j]=temp;
         }
         
-        for(int i=1; i<h-1;++i){
+        for(int i=1; i<h-1;i++){
             int temp=min(left[i-1],right[i+1])-height[i];
             if(temp>0){
                 result+=temp;
